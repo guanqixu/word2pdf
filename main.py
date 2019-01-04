fcec5b4af78d76ba15810e194eccd68e8027555d
guanqixu <guanqixu@qq.com>
2019-01-04 13:02:08 +0800

    add some commnets

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import pdfmerge
 
 
 def doc2pdf(doc_name, pdf_name):
-    """
-    :word文件转pdf
-    :param doc_name word文件名称
-    :param pdf_name 转换后pdf文件名称
+    """ word文件转pdf
+
+    :param doc_name: word文件名称
+    :param pdf_name: 转换后pdf文件名称
+
     """
     try:
         word = client.DispatchEx("Word.Application")
@@ -18,6 +19,7 @@ def doc2pdf(doc_name, pdf_name):
         worddoc = word.Documents.Open(doc_name, ReadOnly=1)
         worddoc.SaveAs(pdf_name, FileFormat=17)
         worddoc.Close()
+        word.Quit()
         return pdf_name
     except Exception as e:
         print(e)
@@ -25,16 +27,24 @@ def doc2pdf(doc_name, pdf_name):
 
 
 def doc2pdf(folder_name):
-    """
-    haha 
+    """ 将文件夹中的 word 全部转 pdf
+
+    :param folder_name: 文件夹名称
+    :return: 转换后 pdf 所在文件夹
     """
     items = [x for x in os.listdir(folder_name)]
+    input = folder_name.split('\\')[-1]
+    output = "output_pdf"
+    output_path = folder_name.replace(input, output)
+    os.makedirs(output_path, exist_ok=True)
+
     try:
         for file in items:
+            # 只遍历 word 文件
             if os.path.splitext(file)[1] == ".docx":
-
                 file_path = "%s\\%s" % (folder_name, file)
-                pdf_path = file_path.replace("docx", "pdf", 1)
+                pdf_path = file_path.replace(
+                    "docx", "pdf").replace(input, output)
                 if os.path.exists(pdf_path):
                     os.remove(pdf_path)
                 word = client.DispatchEx("Word.Application")
@@ -42,17 +52,54 @@ def doc2pdf(folder_name):
                 worddoc = word.Documents.Open(file_path, ReadOnly=1)
                 worddoc.SaveAs(pdf_path, FileFormat=17)
                 worddoc.Close()
+                word.Quit()
+        return output_path
 
     except Exception as e:
         print(e)
-        return 1
+        return ""
+
+
+def mergepdf(foldername):
+    ''' 合并当前文件夹下的所有 pdf 文件
+
+    :param foldername: 文件夹路径
+
+    '''
+
+    files = ["%s\\%s" % (foldername, file) for file in os.listdir(
+        foldername) if os.path.splitext(file)[1] == '.pdf']
+
+    for file in files:
+        pdfmerge.add_blank_page(file)
+
+    pdfmerge.merge(files)
 
 
 if __name__ == '__main__':
-    # foldname = r"F:\Git\mijigenerator\MijiGenerator\bin\Debug\output_doc"
-    # doc2pdf(foldname)
-    file = r"F:\Git\mijigenerator\MijiGenerator\bin\Debug\output_doc\20180416 过敏体质渐露.pdf"
-    folder = r"F:\Git\mijigenerator\MijiGenerator\bin\Debug\output_doc"
-    for file in os.listdir(folder):
-        if os.path.splitext(file)[1] == ".pdf":
-            pdfmerge.add_blank_page(folder + "\\" + file)
+    '''
+    1 为 word 转 pdf
+    2 为 merge 所有 pdf
+    '''
+
+    prompt = '''
+    1 word to pdf
+    2 merge pdf
+    please enter a number:
+    '''
+    while True:
+        num = input(prompt)
+
+        if num == '1':
+            folder = input("Please enter the folder path:")
+            print(doc2pdf(folder))
+
+        elif num == '2':
+            folder = input("Please enter the folder path:")
+            mergepdf(folder)
+
+        elif num == "":
+            break
+
+        else:
+            print("error input")
--- pdfmerge.py
+++ pdfmerge.py
@@ -2,6 +2,13 @@ from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 
 
 def get_reader(filename):
+    ''' 获取 pdfreader
+
+    :param filename: 文件名称
+    :return: pdfreader
+    :rtype: PdfFileReader
+
+    '''
     try:
         pdf_reader = PdfFileReader(filename)
 
@@ -12,6 +19,13 @@ def get_reader(filename):
 
 
 def add_blank_page(filename):
+
+    ''' 添加一个空白页
+
+    只有在页数不是偶数的情况下，才添加
+    :param filename： 文件名
+    
+    '''
     reader = get_reader(filename)
 
     writer = PdfFileWriter()
@@ -22,3 +36,26 @@ def add_blank_page(filename):
         writer.addBlankPage()
 
     writer.write(open(filename, 'wb'))
+
+
+def merge(filenames):
+
+    ''' 合并多个pdf文件
+
+    :param filenames: 文件集合
+    :return: 是否成功合并
+    
+    '''
+
+    merger = PdfFileMerger()
+
+    try:
+        for file in filenames:
+            merger.append(file)
+        merger.write("merge.pdf")
+
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
